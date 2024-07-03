f3814b2d7ad1d67985935e9e53765c2cc260515b
pmj <1205149188@qq.com>
2024-07-03 13:09:08 +0800

    字母异位词分组添加另外一种方法，最长连续序列有两种方法：先排序后遍历、先去重set再遍历

---
+++ hot100/array/ThreeSolution.java
@@ -0,0 +1,77 @@
+package LeetCodeMy.hot100.array;
+import java.util.*;
+
+/**
+ * 最长连续序列：未排序数组nums，找出数字连续的最长序列的长度 设计时间复杂度为O(n)的算法
+ * 
+ * tag：数组、哈希表、并查集
+ */
+public class ThreeSolution {
+    
+    /**
+     * 这个方法最朴素，排序后遍历
+     * 时间复杂度：O(nlogn)  空间复杂度
+     * @param nums
+     * @return
+     */
+    public int longestConsecutive1(int[] nums) {
+        int result = 0;
+        int count = 1;
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        // 先排序
+        Arrays.sort(nums);
+        // 遍历数组中的每个元素，每次+1向后遍历
+        for(int i = 1; i < n; i++) {
+            // 如果当前元素与前一个元素相同，则跳过
+            if(nums[i] == nums[i-1]){
+                continue;
+            }
+            if(nums[i] == nums[i-1] + 1){
+                count++;
+                result = Math.max(count, result);
+            } else {
+                count = 1;
+            }
+
+        }
+        return result;
+    }
+
+    /**
+     * 哈希表：1、先去重；2、
+     * @param nums
+     * @return
+     */
+    public int longestConsecutive2(int[] nums) {
+        // 先去重--使用set集合
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int result = 0; 
+
+        for(int num : nums){
+            if(!set.contains(num-1)){
+                int currentNum = num;
+                int currentIndex = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum = currentNum + 1;
+                    currentIndex = currentIndex + 1;
+                }
+                result = Math.max(result, currentIndex);
+            }
+        }        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,3,7,2,5,8,4,6,0,1};
+        ThreeSolution threeSolution = new ThreeSolution();
+        int result = threeSolution.longestConsecutive1(nums);
+        System.out.println(result);
+    }
+}
--- hot100/array/TwoSolution.java
+++ hot100/array/TwoSolution.java
@@ -10,9 +10,10 @@ import java.util.*;
 public class TwoSolution {
 
     /*
-     * 对每个字符串进行排序，排序后的顺序一样
+     * 排序法：对每个字符串进行排序，排序后的顺序一样
+     * 时间复杂度：O(N∗K∗Log(K))  空间复杂度：O(N∗K)
      */
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] strs) {
         List<List<String>> result = new ArrayList<List<String>>();
         Map<String, List<String>> map = new HashMap<String, List<String>>();
         for(String str: strs){
@@ -24,9 +25,37 @@ public class TwoSolution {
             map.put(key, list);
         }
         result = new ArrayList<List<String>>(map.values());
-        System.out.println(result);
+        // System.out.println(result);
         return result;
     } 
+
+    /*
+     * 计数法：字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的
+     * 时间复杂度：O(N∗K) 空间复杂度：O(N∗K)
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for(String str: strs) {
+            int[] counts = new int[26];
+            int len = str.length();
+            for(int i = 0; i < len; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            // 将每个出现次数大于0的字母和出现次数按顺序拼接成字符串，作为哈希键
+            StringBuffer sb = new StringBuffer();
+            for(int i = 0; i < 26; i++) {
+                if(counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -37,7 +66,7 @@ public class TwoSolution {
             str[i] = sc.nextLine();
         }
         TwoSolution twoSolution = new TwoSolution();
-        twoSolution.groupAnagrams(str);
+        twoSolution.groupAnagrams2(str);
         System.out.println();
         for(int i = 0; i < n; i++){
             System.out.println(str[i]);
